c88db5973a3f34ac177dbaff50e4e85d3efb0b44
George Nishimura <thetmkay@gmail.com>
2014-10-24 03:26:35 +0100

    express app prototyp scaffold

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ * Module dependencies
+ */
+
+  var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    cons = require('consolidate');
+
+  var app = module.exports = express();
+
+
+  /**
+   * Configuration
+   */
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.engine('html', cons.nunjucks);
+  app.set('view engine', 'html');
+  app.use(express.static(path.join(__dirname, 'public')));
+  // app.use(app.router);
+
+  app.get('/', function(req,res) {
+    res.render('index');
+  });
+
+
+  app.get('/commenttree', function(req,res) {
+    res.redirect('http://commenttree.eu01.aws.af.cm/');
+  })
+
