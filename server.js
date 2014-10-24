8da8fdc050be9bc2b9fe73929e97d1f7dbbc53e0
George Nishimura <thetmkay@gmail.com>
2014-10-24 03:30:06 +0100

    adding sub app and server

---
+++ server.js
@@ -0,0 +1,23 @@
+/**
+ * Module dependencies
+ */
+
+var subdomain = require('subdomain'),
+  http = require('http'),
+  path = require('path');
+
+var app = require('./app'),
+      resume = require('./lib/resume/app');
+
+// development only
+// app.use(subdomain({ base : 'localhost', removeWWW : true }));
+// app.get('/subdomain/resume/', resume);
+app.use('/resume', resume);
+
+/**
+ * Start Server
+ */
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
